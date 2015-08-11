6b9265b50f623e5f6c5162477197e0a8ad75931d
Bishal <bishaldhakal268@gmail.com>
2015-08-11 13:02:46 -0700

    YOLO

--- app/scripts/controllers/buy.js
+++ app/scripts/controllers/buy.js
@@ -31,6 +31,29 @@ angular.module('theClosetApp')
       $scope.dressPrice = dataShare.dressPrice;
       $scope.dressName = dataShare.dressName;
       $scope.dressImage = dataShare.dressImage;
+
+ 
+  $scope.ccard1=function(){
+  
+  
+  var numPress=document.getElementById('card').value;
+  if (numPress==3){
+    document.getElementById('img').style.visibility='visible';
+    document.getElementById('img').src='../images/american express.jpg'
+  }
+  if (numPress==4){
+    document.getElementById('img').style.visibility='visible';
+    document.getElementById('img').src='../images/visa.jpg'
+  }
+  if (numPress==5){
+    document.getElementById('img').style.visibility='visible';
+    document.getElementById('img').src='../images/master.jpg';
+  }
+  if (numPress==''){
+    document.getElementById('img').style.visibility='hidden';
+    
+  }}
+
     });
 
   
